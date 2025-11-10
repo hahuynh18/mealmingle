452117ca5c31d88ba5b47dd371deb42f24afefe3
Maximilian Dabski <sapdab@gmail.com>
2025-11-09 21:46:46 -0500

    Initial frontend setup (vulnerabilities must be fixed manually)

---
+++ frontend/src/pages/Inventory.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>Inventory Test Page</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+        </div>
+    );
+}
+
+export default Inventory;
